264276f881ce82f3e7f38a728f1154095da321b1
DeepaliTandale <tandaledeepali99@gmail.com>
2021-03-07 19:21:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -391,64 +391,64 @@ resource "aws_sns_topic_policy" "default" {
 }
 data "aws_caller_identity" "default" {}
 
-data "aws_iam_policy_document" "sns_topic_policy" {
-  policy_id = "__default_policy_ID"
+# data "aws_iam_policy_document" "sns_topic_policy" {
+#   policy_id = "__default_policy_ID"
 
-  statement {
-    sid = "__default_statement_ID"
+#   statement {
+#     sid = "__default_statement_ID"
 
-    actions = [
-      "SNS:Subscribe",
-      "SNS:SetTopicAttributes",
-      "SNS:RemovePermission",
-      "SNS:Receive",
-      "SNS:Publish",
-      "SNS:ListSubscriptionsByTopic",
-      "SNS:GetTopicAttributes",
-      "SNS:DeleteTopic",
-      "SNS:AddPermission",
-    ]
+#     actions = [
+#       "SNS:Subscribe",
+#       "SNS:SetTopicAttributes",
+#       "SNS:RemovePermission",
+#       "SNS:Receive",
+#       "SNS:Publish",
+#       "SNS:ListSubscriptionsByTopic",
+#       "SNS:GetTopicAttributes",
+#       "SNS:DeleteTopic",
+#       "SNS:AddPermission",
+#     ]
 
-    effect    = "Allow"
-    resources = ["${aws_sns_topic.default.arn}"]
+#     effect    = "Allow"
+#     resources = ["${aws_sns_topic.default.arn}"]
 
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
+#     principals {
+#       type        = "AWS"
+#       identifiers = ["*"]
+#     }
 
-    condition {
-      test     = "StringEquals"
-      variable = "AWS:SourceOwner"
+#     condition {
+#       test     = "StringEquals"
+#       variable = "AWS:SourceOwner"
 
-      values = [
-        "${data.aws_caller_identity.default.account_id}",
-      ]
-    }
-  }
+#       values = [
+#         "${data.aws_caller_identity.default.account_id}",
+#       ]
+#     }
+#   }
 
-  statement {
-    sid       = "Allow CloudwatchEvents"
-    actions   = ["sns:Publish"]
-    resources = ["${aws_sns_topic.default.arn}"]
+#   statement {
+#     sid       = "Allow CloudwatchEvents"
+#     actions   = ["sns:Publish"]
+#     resources = ["${aws_sns_topic.default.arn}"]
 
-    principals {
-      type        = "Service"
-      identifiers = ["events.amazonaws.com"]
-    }
-  }
+#     principals {
+#       type        = "Service"
+#       identifiers = ["events.amazonaws.com"]
+#     }
+#   }
 
-  statement {
-    sid       = "Allow RDS Event Notification"
-    actions   = ["sns:Publish"]
-    resources = ["${aws_sns_topic.default.arn}"]
+#   statement {
+#     sid       = "Allow RDS Event Notification"
+#     actions   = ["sns:Publish"]
+#     resources = ["${aws_sns_topic.default.arn}"]
 
-    principals {
-      type        = "Service"
-      identifiers = ["rds.amazonaws.com"]
-    }
-  }
-}
+#     principals {
+#       type        = "Service"
+#       identifiers = ["rds.amazonaws.com"]
+#     }
+#   }
+# }
 
 locals {
   thresholds = {
